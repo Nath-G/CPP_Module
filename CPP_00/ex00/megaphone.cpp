8868e0ed188566ee6b98cec7fde99f0f67c0d828
Nath-G <nathalie.greselin@gmail.com>
2021-01-15 16:32:56 +0000

    header

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/15 16:11:40 by nagresel          #+#    #+#             */
+/*   Updated: 2021/01/15 16:11:50 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int	main(void)
 {
-	std::cout << shhhhh... I think the students are asleep... << std::endl
+	std::cout << "shhhhh... I think the students are asleep..." << std::endl;
 	return (0);
 }

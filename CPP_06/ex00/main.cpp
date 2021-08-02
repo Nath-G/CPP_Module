367320651b8ffb762454a41f8bb645e99586afb0
nagresel <nagresel@student.42.fr>
2021-08-02 12:39:40 +0200

    remove c++98 flag

--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 11:28:48 by nagresel          #+#    #+#             */
-/*   Updated: 2021/08/02 11:50:53 by nagresel         ###   ########.fr       */
+/*   Updated: 2021/08/02 12:37:32 by nagresel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int ac, char **av)
 {
-    if (ac != 2 || av[1] == '\0')
+    if (ac != 2 || av[1] == NULL)
         std::cout << "Error: bad argument." << std::endl;
     else
     {

0a4f064d3314c4a4cbdcf2954d1cac5a67345b15
nagresel <nagresel@student.42.fr>
2021-08-03 18:41:39 +0200

    clean up

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 12:50:56 by nagresel          #+#    #+#             */
-/*   Updated: 2021/08/03 18:31:51 by nagresel         ###   ########.fr       */
+/*   Updated: 2021/08/03 18:41:17 by nagresel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,12 @@ uintptr_t serialize(Data* ptr)
     void *tmp = &ptr;
     uintptr_t *i = reinterpret_cast<uintptr_t*>(tmp);
     ptr->address = *i;
-//    std::cout << "ptr =                         " << ptr << std::endl;
-//    std::cout << "ptr->address decimal value:  " << ptr->address << std::endl;//ptr address decimal value
     return (*i);
 }
 
 Data* deserialize(uintptr_t raw)
 {
-//    std::cout << "raw : " << raw << std::endl;
     Data *dt = reinterpret_cast<Data *>(raw);
-//    std::cout << "dt.address : " << dt->address << std::endl;// this is ptr address decimal value
-//    std::cout << "dt : " << dt << std::endl; //this is raw value, then ptr address 
-
     return(dt);
 }
 
@@ -37,7 +31,7 @@ int main()
 
     Data ptr;
     uintptr_t ptrAddress;
-    std::cout << "ptr address =                 "<< &ptr << std::endl;
+    std::cout << "ptr address:                  "<< &ptr << std::endl;
     ptrAddress = serialize(&ptr);
     Data *ptr2 = deserialize(ptrAddress);
     std::cout << "ptr2 adress:                  " << ptr2 << std::endl;
